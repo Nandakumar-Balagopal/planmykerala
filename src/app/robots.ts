c8c0b24ac07e81650c04eeffe93d4b8730a26c43
Nandakumar B <nandakumarpalaril@gmail.com>
2026-07-30 09:43:29 +0530

    Allow ChatGPT search crawler

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,12 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: '*', allow: '/' }, sitemap: 'https://www.planmykerala.com/sitemap.xml' };
+  return {
+    rules: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: 'Googlebot', allow: '/' },
+      { userAgent: 'OAI-SearchBot', allow: '/' },
+    ],
+    sitemap: 'https://www.planmykerala.com/sitemap.xml',
+  };
 }
